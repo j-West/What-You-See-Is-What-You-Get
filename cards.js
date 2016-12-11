b56d26c3e178a2263250a8407799516f6168da89
j-west <westja89@gmail.com>
2016-12-11 00:32:15 -0600

    Update people array

--- cards.js
+++ cards.js
@@ -1,32 +1,32 @@
-var people = [
+let people = [
                {
                 title: "Entertainer",
                 name: "Andy Bernard",
                 bio: "Andy Bernard because famous when a clip of his audition to 'America's next acapella sensation' went viral.  Andy once led a paper sales office to it's highest daily sales numbers.  His reward, a tattoo on his butt.",
-                image: "https://media0.giphy.com/media/8VrtCswiLDNnO/200_s.gif"
+                image: "https://media0.giphy.com/media/8VrtCswiLDNnO/200_s.gif",
                 lifespan: {
-                  birth: 1747,
-                  death: 1797
+                  birth: 1977,
+                  death: "Living"
                           }
                },
                {
                 title: "Ninja",
                 name: "Diamond Dave",
                 bio: "With special guest appearences on talk show TV, Diamond Dave because famous for his lousy ninja skills.  Most notiably, his ninji star attack, during which he pulls a can of tobacco out of his back pocket and throws it are his adversary.",
-                image: "https://cdn.meme.am/cache/instances/folder335/250x250/59756335.jpg"
+                image: "https://cdn.meme.am/cache/instances/folder335/250x250/59756335.jpg",
                 lifespan: {
-                  birth: 1747,
-                  death: 1797
+                  birth: 1973,
+                  death: 2014
                           }
                },
                {
                 title: "Snake",
-                name: "Slippery Snake",
-                bio: "The exact name of Slippery Snake is still unknown to this day.  Slippery Snake disappeared as mysteriously as Slippery Snake appeared.  What is known, is that Slippery Snake is, well, a Slippery Snake",
-                image: "https://cdn.meme.am/cache/instances/folder857/250x250/57395857.jpg"
+                name: "Slithery Snake",
+                bio: "The exact name of Slithery Snake is still unknown to this day.  Slithery Snake disappeared as mysteriously as Slithery Snake appeared.  What is known, is that Slithery Snake is, well, a Slithery Snake",
+                image: "https://cdn.meme.am/cache/instances/folder857/250x250/57395857.jpg",
                 lifespan: {
-                  birth: unknown,
-                  death: unknown
+                  birth: "unknown",
+                  death: "unknown"
                           }
                },
 
