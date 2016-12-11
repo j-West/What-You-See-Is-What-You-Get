ba7d70b4abad0ff88723f65764889980862e277c
j-west <westja89@gmail.com>
2016-12-10 23:09:43 -0600

    Added html boilerplate elements, created an array of objects

---
+++ cards.js
@@ -0,0 +1,33 @@
+var people = [
+               {
+                title: "Entertainer",
+                name: "Andy Bernard",
+                bio: "Andy Bernard because famous when a clip of his audition to 'America's next acapella sensation' went viral.  Andy once led a paper sales office to it's highest daily sales numbers.  His reward, a tattoo on his butt.",
+                image: "https://media0.giphy.com/media/8VrtCswiLDNnO/200_s.gif"
+                lifespan: {
+                  birth: 1747,
+                  death: 1797
+                          }
+               },
+               {
+                title: "Ninja",
+                name: "Diamond Dave",
+                bio: "With special guest appearences on talk show TV, Diamond Dave because famous for his lousy ninja skills.  Most notiably, his ninji star attack, during which he pulls a can of tobacco out of his back pocket and throws it are his adversary.",
+                image: "https://cdn.meme.am/cache/instances/folder335/250x250/59756335.jpg"
+                lifespan: {
+                  birth: 1747,
+                  death: 1797
+                          }
+               },
+               {
+                title: "Snake",
+                name: "Slippery Snake",
+                bio: "The exact name of Slippery Snake is still unknown to this day.  Slippery Snake disappeared as mysteriously as Slippery Snake appeared.  What is known, is that Slippery Snake is, well, a Slippery Snake",
+                image: "https://cdn.meme.am/cache/instances/folder857/250x250/57395857.jpg"
+                lifespan: {
+                  birth: unknown,
+                  death: unknown
+                          }
+               },
+
+             ]
